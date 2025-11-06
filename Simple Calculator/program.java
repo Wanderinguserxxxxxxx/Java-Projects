52cbaf30cc2150f280899ef2787bf4c742f3dc92
Wanderinguserxxxxxxx <sahilkumar3570218@gmail.com>
2025-11-06 23:53:33 +0530

    used panels and added more operation

--- Simple Calculator/program.java
+++ Simple Calculator/program.java
@@ -1,127 +1,107 @@
-import java.awt.Color;
-
 import javax.swing.*;
+import java.awt.*;
 
-class GUI_cal extends JFrame {
-    GUI_cal() {
-        setTitle("Simple Calculator");
+abstract class PanelImplementation extends JFrame {
+    protected JPanel panel, panel2, panel3;
+    protected JTextField t1, t2;
+    protected JLabel result;
+
+    PanelImplementation() {
+        // --- Input Panel ---
+        panel = new JPanel(new GridLayout(2, 2, 20, 10));
+        JLabel l1 = new JLabel("Enter first number: ");
+        JLabel l2 = new JLabel("Enter second number: ");
+        t1 = new JTextField();
+        t2 = new JTextField();
+        panel.add(l1);
+        panel.add(t1);
+        panel.add(l2);
+        panel.add(t2);
+        panel.setBackground(Color.ORANGE);
+
+        // --- Buttons Panel ---
+        panel2 = new JPanel(new GridLayout(3, 3, 20, 10));
+        JButton bAdd = new JButton("ADD");
+        JButton bSub = new JButton("SUBTRACT");
+        JButton bMul = new JButton("MULTIPLY");
+        JButton bDiv = new JButton("DIVIDE");
+        JButton bMod = new JButton("MODULUS");
+        JButton bSq = new JButton("SQUARE");
+        JButton bClear = new JButton("CLEAR");
+        JButton bExit = new JButton("EXIT");
+
+        // Add buttons neatly
+        panel2.add(bAdd);
+        panel2.add(bSub);
+        panel2.add(bMul);
+        panel2.add(bDiv);
+        panel2.add(bMod);
+        panel2.add(bSq);
+        panel2.add(bClear);
+        panel2.add(bExit);
+
+        // --- Result Panel ---
+        panel3 = new JPanel(new GridLayout(1, 1, 20, 20));
+        result = new JLabel("RESULT WILL BE SHOWN HERE", SwingConstants.CENTER);
+        result.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+        panel3.add(result);
+
+        // --- Button Actions ---
+        bAdd.addActionListener(e -> calculate("add"));
+        bSub.addActionListener(e -> calculate("sub"));
+        bMul.addActionListener(e -> calculate("mul"));
+        bDiv.addActionListener(e -> calculate("div"));
+        bMod.addActionListener(e -> calculate("mod"));
+        bSq.addActionListener(e -> calculate("sq"));
+
+        //  Clear button
+        bClear.addActionListener(e -> {
+            t1.setText("");
+            t2.setText("");
+            result.setText("RESULT WILL BE SHOWN HERE");
+        });
+
+        // Exit button
+        bExit.addActionListener(e -> System.exit(0));
+    }
+
+    // --- Helper method to avoid repetition ---
+    protected void calculate(String op) {
+        try {
+            int num1 = Integer.parseInt(t1.getText().trim());
+            int num2 = Integer.parseInt(t2.getText().trim());
+
+            switch (op) {
+                case "add" -> result.setText("Sum: " + (num1 + num2));
+                case "sub" -> result.setText("Difference: " + (num1 - num2));
+                case "mul" -> result.setText("Product: " + (num1 * num2));
+                case "div" -> result.setText(num2 == 0 ? "Can't divide by 0" : "Quotient: " + (num1 / num2));
+                case "mod" -> result.setText("Modulus: " + (num1 % num2));
+                case "sq" -> result.setText("Square of " + num1 + " = " + (num1 * num1) +
+                                            " | Square of " + num2 + " = " + (num2 * num2));
+            }
+        } catch (NumberFormatException ex) {
+            result.setText("Enter valid numbers!");
+        }
+    }
+}
+
+class calculator_onceAgain extends PanelImplementation {
+    calculator_onceAgain() {
         setSize(600, 800);
-        setLayout(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setBackground(Color.yellow);
-
-        JLabel leb1 = new JLabel("Number 1:");
-        leb1.setBounds(120, 120, 80, 100);
-        add(leb1);
-        JLabel leb2 = new JLabel("Number 2:");
-        leb2.setBounds(120, 165, 80, 100);
-        add(leb2);
-
-        JTextField field1 = new JTextField();
-        field1.setBounds(220, 145, 80, 40);
-        add(field1);
-        JTextField field2 = new JTextField();
-        field2.setBounds(220, 195, 80, 40);
-        add(field2);
-
-        JLabel result = new JLabel("Result here");
-        result.setBounds(330, 165, 180, 40);
-        add(result);
-
-        JButton sumButton = new JButton("ADD");
-        sumButton.setBounds(170, 245, 80, 40);
-        add(sumButton);
-
-        JButton DiffButton = new JButton("Diff");
-        DiffButton.setBounds(170, 295, 80, 40);
-        add(DiffButton);
-
-        JButton ProdButton = new JButton("Multiply");
-        ProdButton.setBounds(255, 245, 100, 40);
-        add(ProdButton);
-
-        JButton DiviButton = new JButton("Divide");
-        DiviButton.setBounds(255, 295, 100, 40);
-        add(DiviButton);
-
-        JButton ModButton = new JButton("Modulus");
-        ModButton.setBounds(200 ,345, 100, 40);
-        add(ModButton);
-
-        sumButton.addActionListener(e -> {
-            try {
-                int num1 = Integer.parseInt(field1.getText());
-                int num2 = Integer.parseInt(field2.getText());
-                int sum = num1 + num2;
-
-                result.setText("The Sum is: " + sum);
-            } catch (NumberFormatException e1) {
-                result.setText("Enter valid number");
-            }
-        });
-        DiffButton.addActionListener(e -> {
-            try {
-                int num1 = Integer.parseInt(field1.getText());
-                int num2 = Integer.parseInt(field2.getText());
-                int Diff = num1 - num2;
-
-                result.setText("The Difference is: " + Diff);
-            } catch (NumberFormatException e2) {
-                result.setText("Enter Valid number");
-            }
-        });
-        ProdButton.addActionListener(e -> {
-            try {
-                int num1 = Integer.parseInt(field1.getText());
-                int num2 = Integer.parseInt(field2.getText());
-                int Product = num1 * num2;
-
-                result.setText("The Product is: " + Product);
-            } catch (NumberFormatException e2) {
-                result.setText("Enter Valid number");
-            }
-        });
-        DiviButton.addActionListener(e -> {
-            try {
-                int num1 = Integer.parseInt(field1.getText());
-                int num2 = Integer.parseInt(field2.getText());
-
-                if (num2 == 0) {
-                    result.setText("Cannot divide by zero!");
-                } else {
-                    int Divide = num1 / num2;
-                    result.setText("The quotient is: " + Divide);
-                }
-            } catch (NumberFormatException e2) {
-                result.setText("Enter valid numbers!");
-            } catch (Exception e3) {
-                result.setText("Something went wrong!");
-            }
-        });
-        ModButton.addActionListener(e -> {
-            try {
-                int num1 = Integer.parseInt(field1.getText());
-                int num2 = Integer.parseInt(field2.getText());
-
-                if (num2 == 0) {
-                    result.setText("Cannot mod by zero!");
-                } else {
-                    int modulus = num1 % num2;
-                    result.setText("The remainder is: " + modulus);
-                }
-            } catch (NumberFormatException e2) {
-                result.setText("Enter valid numbers!");
-            } catch (Exception e3) {
-                result.setText("Something went wrong!");
-            }
-        });
-
-        setVisible(true);// always in last
+        setLayout(new FlowLayout(FlowLayout.CENTER, 60, 40));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        getContentPane().setBackground(Color.ORANGE);
+        setTitle("CALCULATOR ONCE AGAIN");
+        add(panel);
+        add(panel2);
+        add(panel3);
+        setVisible(true);
     }
 }
 
 public class program {
     public static void main(String[] args) {
-        new GUI_cal();
+        new calculator_onceAgain();
     }
 }
